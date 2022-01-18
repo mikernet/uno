5b01aeb7217a72dd7b1cc717b5f0954afbcb2955
Agnès Zitte <agnes.zitte@nventive.com>
2022-01-18 15:08:19 -0500

    Test - UI Tests for iOS

--- src/SamplesApp/UITests.Shared/Windows_UI_Input/PointersTests/HitTest_LightDismiss.xaml.cs
+++ src/SamplesApp/UITests.Shared/Windows_UI_Input/PointersTests/HitTest_LightDismiss.xaml.cs
@@ -27,93 +27,93 @@ namespace UITests.Windows_UI_Input.PointersTests
 	[Sample("Pointers", "Popup")]
 	public sealed partial class HitTest_LightDismiss : Page
 	{
-		private Popup _popup;
+		//private Popup _popup;
 		public HitTest_LightDismiss()
 		{
 			this.InitializeComponent();
 
-			var popupChild = new Border
-			{
-				Width = 220,
-				Height = 140,
-				Background = new SolidColorBrush(Colors.DarkGreen),
-				Child = new TextBlock
-				{
-					Text = "X",
-					HorizontalAlignment = HorizontalAlignment.Center,
-					VerticalAlignment = VerticalAlignment.Center,
-				}
-			};
-			_popup = new Popup
-			{
-				Child = popupChild
-			};
-			AutomationProperties.SetAutomationId(popupChild, "TargetPopupContent");
-			popupChild.PointerPressed += (o, e) => ResultTextBlock.Text = "Popup content pressed";
-
-			void OnFlyoutOpenedOrClosed(object sender, object args)
-			{
-				FlyoutStatusTextBlock.Text = ((Flyout)sender).IsOpen.ToString();
-			}
-			ButtonFlyout.Opened += OnFlyoutOpenedOrClosed;
-			ButtonFlyout.Closed += OnFlyoutOpenedOrClosed;
-
-			void OnPopupOpenedOrClosed(object sender, object args)
-			{
-				PopupStatusTextBlock.Text = ((Popup)sender).IsOpen.ToString();
-			}
-			_popup.Opened += OnPopupOpenedOrClosed;
-			_popup.Closed += OnPopupOpenedOrClosed;
-
-			TargetComboBox.SelectionChanged += (o, e) =>
-			{
-				ResultTextBlock.Text = $"Item selected";
-			};
-		}
-
-		private void LaunchDismissiblePopup(object sender, object args)
-		{
-			if (!_popup.IsOpen)
-			{
-				_popup.IsLightDismissEnabled = true;
-				_popup.IsOpen = true;
-			}
-		}
-
-		private void LaunchUndismissiblePopup(object sender, object args)
-		{
-			if (!_popup.IsOpen)
-			{
-				_popup.IsLightDismissEnabled = false;
-				_popup.IsOpen = true;
-			}
-		}
-
-		private void ResetResult(object sender, object args)
-		{
-			ResultTextBlock.Text = "None";
-			ButtonFlyout.Hide();
-			_popup.IsOpen = false;
-			TargetComboBox.SelectedItem = null;
-		}
-
-		private void DoAction(object sender, object args)
-		{
-			//if (!_popup.IsLightDismissEnabled && _popup.IsOpen)
+			//var popupChild = new Border
 			//{
-			//	ResultTextBlock.Text = "Popup closed";
-			//	_popup.IsOpen = false;
+			//	Width = 220,
+			//	Height = 140,
+			//	Background = new SolidColorBrush(Colors.DarkGreen),
+			//	Child = new TextBlock
+			//	{
+			//		Text = "X",
+			//		HorizontalAlignment = HorizontalAlignment.Center,
+			//		VerticalAlignment = VerticalAlignment.Center,
+			//	}
+			//};
+			//_popup = new Popup
+			//{
+			//	Child = popupChild
+			//};
+			//AutomationProperties.SetAutomationId(popupChild, "TargetPopupContent");
+			//popupChild.PointerPressed += (o, e) => ResultTextBlock.Text = "Popup content pressed";
+
+			//void OnFlyoutOpenedOrClosed(object sender, object args)
+			//{
+			//	FlyoutStatusTextBlock.Text = ((Flyout)sender).IsOpen.ToString();
 			//}
-			//else
-			{
-				ResultTextBlock.Text = "Button pressed";
+			//ButtonFlyout.Opened += OnFlyoutOpenedOrClosed;
+			//ButtonFlyout.Closed += OnFlyoutOpenedOrClosed;
 
-			}
+			//void OnPopupOpenedOrClosed(object sender, object args)
+			//{
+			//	PopupStatusTextBlock.Text = ((Popup)sender).IsOpen.ToString();
+			//}
+			//_popup.Opened += OnPopupOpenedOrClosed;
+			//_popup.Closed += OnPopupOpenedOrClosed;
+
+			//TargetComboBox.SelectionChanged += (o, e) =>
+			//{
+			//	ResultTextBlock.Text = $"Item selected";
+			//};
 		}
 
-		private void OnFlyoutContentPressed(object sender, object args)
-		{
-				ResultTextBlock.Text = "Flyout content pressed";
-		}
+		//private void LaunchDismissiblePopup(object sender, object args)
+		//{
+		//	if (!_popup.IsOpen)
+		//	{
+		//		_popup.IsLightDismissEnabled = true;
+		//		_popup.IsOpen = true;
+		//	}
+		//}
+
+		//private void LaunchUndismissiblePopup(object sender, object args)
+		//{
+		//	if (!_popup.IsOpen)
+		//	{
+		//		_popup.IsLightDismissEnabled = false;
+		//		_popup.IsOpen = true;
+		//	}
+		//}
+
+		//private void ResetResult(object sender, object args)
+		//{
+		//	ResultTextBlock.Text = "None";
+		//	ButtonFlyout.Hide();
+		//	_popup.IsOpen = false;
+		//	TargetComboBox.SelectedItem = null;
+		//}
+
+		//private void DoAction(object sender, object args)
+		//{
+		//	//if (!_popup.IsLightDismissEnabled && _popup.IsOpen)
+		//	//{
+		//	//	ResultTextBlock.Text = "Popup closed";
+		//	//	_popup.IsOpen = false;
+		//	//}
+		//	//else
+		//	{
+		//		ResultTextBlock.Text = "Button pressed";
+
+		//	}
+		//}
+
+		//private void OnFlyoutContentPressed(object sender, object args)
+		//{
+		//		ResultTextBlock.Text = "Flyout content pressed";
+		//}
 	}
 }
